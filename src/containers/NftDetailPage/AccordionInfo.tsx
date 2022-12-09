9bb06b577e37836fb9912f8baf2d380b4940d68b
renkov <miguel.trabajomadrid@gmail.com>
2022-12-09 11:32:10 +0100

    first commit

---
+++ src/containers/NftDetailPage/AccordionInfo.tsx
@@ -0,0 +1,59 @@
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
+
+export default function AccordionInfo() {
+  return (
+    <div className="w-full rounded-2xl">
+      <Disclosure defaultOpen>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-500 rounded-lg hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75">
+              <span>Descriptions</span>
+              <ChevronUpIcon
+                className={`${
+                  open ? "transform rotate-180" : ""
+                } w-5 h-5 text-neutral-500`}
+              />
+            </Disclosure.Button>
+            <Disclosure.Panel
+              className="px-4 pt-4 pb-2 text-neutral-500 text-sm dark:text-neutral-400"
+              as="p"
+            >
+              Nuestro exclusivo desarrollo inmobiliario FlamencosCity con 68Ha de superficie privada incluye una gran laguna de 2,5ha de espejo de agua con más de 8.000 m2 de arena de playa. Conformado de casas con terrenos y apartamentos.
+              <br></br>
+              Usted podra formalizar la escritura en el apartado “Escriturar” de la plataforma, o si lo prefiere comercializar con el en un mercado secundario.
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      <Disclosure defaultOpen as="div" className="mt-5 md:mt-8">
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 font-medium text-left bg-neutral-100 dark:bg-neutral-700 dark:hover:bg-neutral-500 rounded-lg hover:bg-neutral-200 focus:outline-none focus-visible:ring focus-visible:ring-neutral-500 focus-visible:ring-opacity-75">
+              <span>Details</span>
+              <ChevronUpIcon
+                className={`${
+                  open ? "transform rotate-180" : ""
+                } w-5 h-5 text-neutral-500`}
+              />
+            </Disclosure.Button>
+            <Disclosure.Panel className="px-4 pt-4 pb-2 flex flex-col text-xs text-neutral-500 dark:text-neutral-400 overflow-hidden">
+              <span>2000 x 2000 px.IMAGE(685KB)</span>
+              <br />
+              <span>Contract Address</span>
+              <span className="text-base text-neutral-900 dark:text-neutral-100 line-clamp-1">
+                0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a
+              </span>
+
+              <br />
+              <span>Token ID</span>
+              <span className="text-base text-neutral-900 dark:text-neutral-100">
+                100300372864
+              </span>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  );
+}
